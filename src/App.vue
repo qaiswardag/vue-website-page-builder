3b8d0f848378ecf9bd6d652c3afe4ad63fcbe02f
qaiswardag <qais.wardag@outlook.com>
2024-07-18 22:05:27 +0200

    Install Vue & Tailwind CSS

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <p class="pt-12 text-red-400">Vue 3</p>
+</template>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.use(createPinia());
+
+app.mount('#app');
