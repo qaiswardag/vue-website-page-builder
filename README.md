cdebb3bd53c9ebbde2cf1cb7371e8694ec062f1f
qaiswardag <qais.wardag@outlook.com>
2024-08-20 17:18:17 +0200

    Bug fixes

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
+<p align="center" dir="auto">
+<img width="200" style="max-width: 100%;" src="./public/logo/logo.svg" alt="Logo">
+</p>
+
 # Free Vue Click & Drop Page Builder
 
-Demo [play around](https://www.demo.myissue.dk)
+To star the repository, simply click on the **Star** button located at the top-right corner of the GitHub page. Thank you in advance for your support! 🙌
 
 ## Introduction
+
+If you find this project useful or interesting, I would greatly appreciate if you could star ⭐ the [GitHub repository](https://github.com/qaiswardag/vue-website-page-builder). It helps to boost the project's visibility and encourages me to continue adding new features.
+
+## About and demo
+
+This app was born out of my desire to make a minimalst page builder with a elegant and intuitive design.
+
+[Play around with the page builder](https://www.builder-demo.myissue.dk)
 
 ### About
 
@@ -52,6 +64,90 @@ yarn install
 # bun
 bun install
 ```
+
+# old one start
+
+# old one start
+
+# old one start
+
+# old one start
+
+<p align="center" dir="auto">
+<img width="200" style="max-width: 100%;" src="public/logo-myissue.svg" alt="Logo">
+</p>
+
+# Intro
+
+## Technologies and Frameworks
+
+Built using cutting-Edge Technologies and Frameworks.
+Developed with Vue 3, Vite, Vuex and Tailwind CSS.
+
+# Installation
+
+## Getting started
+
+Clone the repository locally:
+
+```
+git clone https://github.com/qaiswardag/vue-website-page-builder.git
+```
+
+Install packages:
+
+```
+npm install
+```
+
+Run App
+
+```
+npm run dev
+```
+
+## Required Software Installation
+
+Please note that these instructions assume you have PHP, Composer, and Node.js installed
+
+# Features
+
+- Live Drag & Drop Builder: Easily drag and drop content on a page to bring it to life.
+
+- Technologies and Frameworks: Built using cutting-edge technologies like Vue 3, Vuex, Composition API, and Tailwind CSS.
+
+- Set Brand and Link Colors in One Place: Centralize the color settings and apply them across the entire platform effortlessly.
+
+- Mobile-First Approach: Developed with a Mobile-First approach to ensure a seamless experience on various devices.
+
+- Media Library: Enjoy a beautiful and user-friendly media library for easy image management and updates.
+
+- Minimal and Intuitive Design: Engage users with an elegant and intuitive design that enhances their visual experience.
+
+# Documentation
+
+### Customizing the page builder
+
+I have separated all the logic for manipulating the HTML elements into its own Designer class, which can be found at the path: `src/composables/PageBuilder.js`.
+
+Customizing the page builder is made simple since all the logic resides in the Designer Class.
+
+### Unsplash
+
+Please note that if you want to use Unsplash, simply create an .env file in your root folder and enter your Unsplash API key and value.
+
+Example: VITE_UNSPLASH_KEY="your-unsplash-api-key-here"
+
+[Get your unsplash api key here](https://unsplash.com/developers).
+
+# Use with Backend
+
+The Page builder's capabilities become infinite when integrated with a backend framework like Laravel or Express.js.
+
+If you are familiar with Laravel, you can integrate the page builder with my open-source Laravel Admin Panel available at:
+[Demo & repo](https://github.com/qaiswardag/myissue-admin).
+
+By utilizing a backend Framework, the HTML components, currently stored in a JSON file at `public/components.json`, can be easily stored in the database, resulting in better management of HTML components.
 
 ### Technical Details
 
--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,8 @@ import PageBuilderModal from '@/Components/Modals/PageBuilderModal.vue';
 import PageBuilderView from '@/PageBuilder/PageBuilder.vue';
 import PageBuilder from '@/composables/PageBuilder';
 import { delay } from '@/helpers/delay';
-import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
-import { parseISO, formatISO, format } from 'date-fns';
 import FullScreenSpinner from '@/Components/Loaders/FullScreenSpinner.vue';
-
-import {
-  Listbox,
-  ListboxButton,
-  ListboxLabel,
-  ListboxOption,
-  ListboxOptions,
-} from '@headlessui/vue';
 
 const openDesignerModal = ref(false);
 
@@ -146,15 +136,61 @@ const handleDraftForUpdate = async function () {
     <PageBuilderView></PageBuilderView>
   </PageBuilderModal>
 
+  <nav
+    class="px-6 py-6 mx-auto flex items-center justify-between bg-gray-50"
+    aria-label="Global"
+  >
+    <a href="/">
+      <img
+        class="h-6"
+        src="/logo/logo.svg"
+        alt="Logo"
+      />
+    </a>
+
+    <div class="flex lg:gap-x-12 myPrimaryGap">
+      <p
+        @click="handlePageBuilder"
+        class="text-sm font-medium text-myPrimaryDarkGrayColor flex gap-2 items-center cursor-pointer"
+      >
+        Start Builder
+      </p>
+    </div>
+  </nav>
+
+  <div class="myPrimaryMainPageHeaderParagraph">
+    <h1 class="myPrimaryMainPageHeaderNotLoggedIn">
+      Build stunning pages with Vue
+      <br />
+      using an easy interface
+    </h1>
+    <p class="myPrimaryMainPageParagraphNotLoggedIn">
+      Power your vision and build impressive pages with ease using an intuitive
+      Vue.js drag-and-drop page builder.
+      <br />
+      <a
+        class="text-myPrimaryLinkColor"
+        href="https://github.com/qaiswardag/vue-website-page-builder"
+        target="_blank"
+      >
+        GitHub repository.
+      </a>
+    </p>
+    <div class="flex justify-center gap-2 my-8">
+      <div>
+        <button
+          @click="handlePageBuilder"
+          class="myPrimaryButton"
+        >
+          Start Builder
+        </button>
+      </div>
+    </div>
+  </div>
   <div class="myPrimaryContentSection">
-    <h2 class="mySecondaryHeader">
-      <!--[-->Fashion brands in one searchable solution<!--]-->
-    </h2>
     <p class="myPrimaryParagraph font-normal">
-      <!--[-->
       myself is the largest and most exclusive fashion directory in the United
       Arab Emirates, gathering all fashion brands in one searchable solution.
-      <!--]-->
     </p>
   </div>
   <div class="myPrimaryContentSection">
@@ -577,4 +613,44 @@ const handleDraftForUpdate = async function () {
       <!--]-->
     </div>
   </div>
+  <div
+    class="space-y-6 mx-auto px-6 py-4 md:flex md:items-center md:justify-between lg:px-8 bg-myPrimaryLightGrayColor"
+  >
+    <div
+      class="flex justify-center space-x-6 md:order-2 text-gray-600 hover:text-gray-700"
+    >
+      <p class="myPrimaryParagraph text-gray-600">
+        ©
+        <a
+          href="https://github.com/qaiswardag/vue-website-page-builder"
+          target="_blank"
+          class="myPrimaryLink text-gray-600"
+          >Qais Wardag / GitHub
+        </a>
+      </p>
+    </div>
+    <div
+      class="flex justify-center space-x-6 md:order-2 text-gray-600 hover:text-gray-700"
+    >
+      <a
+        href="https://github.com/qaiswardag/vue-website-page-builder"
+        target="_blank"
+      >
+        <span class="sr-only">GitHub</span>
+        <svg
+          class="h-6 w-6"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+          aria-hidden="true"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </a>
+    </div>
+  </div>
+  <!-- Footer end -->
 </template>
