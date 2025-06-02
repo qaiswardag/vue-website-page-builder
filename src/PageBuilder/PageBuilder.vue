9241d837ae440d1a7ea3d40e96eb1f6bc393ccf4
Qais Wardag <qais.wardag@outlook.com>
2025-06-02 11:04:07 +0300

    Improve component layout in PageBuilder.vue

--- src/PageBuilder/PageBuilder.vue
+++ src/PageBuilder/PageBuilder.vue
@@ -111,73 +111,29 @@ onMounted(async () => {
 </script>
 
 <template>
-  <SearchComponents
-    v-if="showModalAddComponent"
-    :show="showModalAddComponent"
-    :firstButtonText="firstButtonTextSearchComponents"
-    :title="titleModalAddComponent"
-    @firstModalButtonSearchComponentsFunction="firstModalButtonSearchComponentsFunction"
-  ></SearchComponents>
-  <PageBuilderPreviewModal
-    :show="openPageBuilderPreviewModal"
-    @firstPageBuilderPreviewModalButton="firstPageBuilderPreviewModalButton"
-  >
-    <Preview></Preview>
-  </PageBuilderPreviewModal>
+  <div>
+    <SearchComponents
+      v-if="showModalAddComponent"
+      :show="showModalAddComponent"
+      :firstButtonText="firstButtonTextSearchComponents"
+      :title="titleModalAddComponent"
+      @firstModalButtonSearchComponentsFunction="firstModalButtonSearchComponentsFunction"
+    ></SearchComponents>
+    <PageBuilderPreviewModal
+      :show="openPageBuilderPreviewModal"
+      @firstPageBuilderPreviewModalButton="firstPageBuilderPreviewModalButton"
+    >
+      <Preview></Preview>
+    </PageBuilderPreviewModal>
 
-  <div class="w-full inset-x-0 h-[90vh] z-10 bg-white overflow-x-scroll lg:pt-2 pt-2">
-    <div class="relative h-full flex">
-      <div
-        @click.self="pageBuilderStateStore.setComponent(null)"
-        class="min-w-[3.5rem] pt-6 pb-2 my-2 mx-2 bg-myPrimaryLightGrayColor rounded-full shadow"
-      >
-        <div class="mx-2 flex flex-col myPrimaryGap">
-          <div class="flex gap-2 items-center justify-center">
-            <button
-              type="button"
-              @click="
-                () => {
-                  pageBuilderStateStore.setComponentArrayAddMethod('unshift')
-                  handleAddComponent()
-                }
-              "
-              class="h-10 w-10 cursor-pointer rounded-full flex items-center border-none justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white focus-visible:ring-0"
-            >
-              <span class="myMediumIcon material-symbols-outlined"> add </span>
-            </button>
-          </div>
-          <div @click.self="pageBuilderStateStore.setComponent(null)">
-            <ComponentTopMenu v-if="getElement"></ComponentTopMenu>
-          </div>
-        </div>
-      </div>
-      <main class="flex flex-col h-full grow rounded-2xl duration-300 shadow-2xl">
+    <div class="w-full inset-x-0 h-[90vh] z-10 bg-white overflow-x-scroll lg:pt-2 pt-2">
+      <div class="relative h-full flex">
         <div
-          class="flex items-center justify-between primary-gap rounded-t-2xl bg-myPrimaryLightGrayColor"
+          @click.self="pageBuilderStateStore.setComponent(null)"
+          class="min-w-[3.5rem] pt-6 pb-2 my-2 mx-2 bg-myPrimaryLightGrayColor rounded-full shadow"
         >
-          <div
-            @click.self="pageBuilderStateStore.setComponent(null)"
-            class="w-4/12 flex justify-start items-center py-2 pl-2 h-full"
-          >
-            <div class="flex gap-2">
-              <span class="w-2 h-2 rounded-full bg-red-400"></span>
-              <span class="w-2 h-2 rounded-full bg-yellow-400"></span>
-              <span class="w-2 h-2 rounded-full bg-green-400"></span>
-            </div>
-          </div>
-
-          <div
-            @click.self="pageBuilderStateStore.setComponent(null)"
-            class="w-4/12 flex justify-center py-2"
-          >
-            <OptionsDropdown @previewCurrentDesign="previewCurrentDesign"></OptionsDropdown>
-          </div>
-
-          <div
-            @click.self="pageBuilderStateStore.setComponent(null)"
-            class="w-4/12 flex justify-end py-2 pr-2"
-          >
-            <div class="flex items-center justify-center gap-4">
+          <div class="mx-2 flex flex-col myPrimaryGap">
+            <div class="flex gap-2 items-center justify-center">
               <button
                 type="button"
                 @click="
@@ -186,104 +142,150 @@ onMounted(async () => {
                     handleAddComponent()
                   }
                 "
-              >
-                <div class="flex items-center justify-center gap-2">
-                  <span class="lg:block hidden"> Add new Component </span>
-
-                  <span
-                    class="h-10 w-10 cursor-pointer rounded-full flex items-center border-none justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white focus-visible:ring-0"
-                  >
-                    <span class="myMediumIcon material-symbols-outlined"> add </span>
-                  </span>
-                </div>
-              </button>
-              <button
-                type="button"
-                @click="
-                  () => {
-                    pageBuilderStateStore.setMenuRight(false)
-                    pageBuilderStateStore.setElement(null)
-                    pageBuilderStateStore.setComponent(null)
-                    handlePageBuilderPreview()
-                  }
-                "
                 class="h-10 w-10 cursor-pointer rounded-full flex items-center border-none justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white focus-visible:ring-0"
               >
-                <span class="material-symbols-outlined"> visibility </span>
+                <span class="myMediumIcon material-symbols-outlined"> add </span>
               </button>
-
-              <button
-                type="button"
-                v-if="getMenuRight === false"
-                @click="pageBuilderStateStore.setMenuRight(true)"
-                class="h-10 w-10 cursor-pointer rounded-full flex items-center border-none justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white focus-visible:ring-0"
-              >
-                <span class="material-symbols-outlined"> gesture </span>
-              </button>
+            </div>
+            <div @click.self="pageBuilderStateStore.setComponent(null)">
+              <ComponentTopMenu v-if="getElement"></ComponentTopMenu>
             </div>
           </div>
         </div>
-
-        <EditGetElement></EditGetElement>
-        <div
-          @click="pageBuilderStateStore.setComponent(null)"
-          id="contains-pagebuilder"
-          class="pl-4 pr-4 pb-4 overflow-y-auto h-screen pt-1"
-        >
-          <div id="pagebuilder">
-            <div ref="draggableZone">
-              <!-- Added Components to DOM # start -->
-              <div
-                v-for="component in Array.isArray(getComponents) && getComponents"
-                :key="component"
-                id="page-builder-editor-editable-area"
-                class="bg-white grow"
-              >
-                <div @mouseup="handleSelectComponent(component)" class="relative group">
-                  <div v-html="component.html_code"></div>
-                </div>
+        <main class="flex flex-col h-full grow rounded-2xl duration-300 shadow-2xl">
+          <div
+            class="flex items-center justify-between primary-gap rounded-t-2xl bg-myPrimaryLightGrayColor"
+          >
+            <div
+              @click.self="pageBuilderStateStore.setComponent(null)"
+              class="w-4/12 flex justify-start items-center py-2 pl-2 h-full"
+            >
+              <div class="flex gap-2">
+                <span class="w-2 h-2 rounded-full bg-red-400"></span>
+                <span class="w-2 h-2 rounded-full bg-yellow-400"></span>
+                <span class="w-2 h-2 rounded-full bg-green-400"></span>
               </div>
             </div>
-            <!-- Added Components to DOM # end -->
 
-            <!-- Add Component # start -->
             <div
-              class="rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 my-12 mx-8"
+              @click.self="pageBuilderStateStore.setComponent(null)"
+              class="w-4/12 flex justify-center py-2"
             >
-              <h3 class="mt-2 text-sm font-medium text-gray-900">Add Components</h3>
-              <p class="mt-1 text-sm text-gray-500">
-                Get started by adding components using the drag & drop Page Builder.
-              </p>
-              <div class="mt-6 flex items-center gap-2 justify-center">
+              <OptionsDropdown @previewCurrentDesign="previewCurrentDesign"></OptionsDropdown>
+            </div>
+
+            <div
+              @click.self="pageBuilderStateStore.setComponent(null)"
+              class="w-4/12 flex justify-end py-2 pr-2"
+            >
+              <div class="flex items-center justify-center gap-4">
                 <button
+                  type="button"
                   @click="
                     () => {
-                      pageBuilderStateStore.setComponentArrayAddMethod('push')
+                      pageBuilderStateStore.setComponentArrayAddMethod('unshift')
                       handleAddComponent()
                     }
                   "
-                  type="button"
-                  class="myPrimaryButton flex items-center gap-2 justify-center"
                 >
-                  <span class="myMediumIcon material-symbols-outlined"> add </span>
-                  Add component
+                  <div class="flex items-center justify-center gap-2">
+                    <span class="lg:block hidden"> Add new Component </span>
+
+                    <span
+                      class="h-10 w-10 cursor-pointer rounded-full flex items-center border-none justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white focus-visible:ring-0"
+                    >
+                      <span class="myMediumIcon material-symbols-outlined"> add </span>
+                    </span>
+                  </div>
+                </button>
+                <button
+                  type="button"
+                  @click="
+                    () => {
+                      pageBuilderStateStore.setMenuRight(false)
+                      pageBuilderStateStore.setElement(null)
+                      pageBuilderStateStore.setComponent(null)
+                      handlePageBuilderPreview()
+                    }
+                  "
+                  class="h-10 w-10 cursor-pointer rounded-full flex items-center border-none justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white focus-visible:ring-0"
+                >
+                  <span class="material-symbols-outlined"> visibility </span>
+                </button>
+
+                <button
+                  type="button"
+                  v-if="getMenuRight === false"
+                  @click="pageBuilderStateStore.setMenuRight(true)"
+                  class="h-10 w-10 cursor-pointer rounded-full flex items-center border-none justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white focus-visible:ring-0"
+                >
+                  <span class="material-symbols-outlined"> gesture </span>
                 </button>
               </div>
             </div>
-            <!-- Add Component # end -->
           </div>
-        </div>
-        <!-- Add Component # end -->
-      </main>
 
-      <aside
-        aria-label="Menu"
-        :class="{ 'w-0': !getMenuRight, 'w-80 ml-4': getMenuRight }"
-        class="h-full duration-300 z-20 flex-shrink-0 overflow-hidden shadow-2xl rounded-l-2xl bg-white"
-      >
-        <RightSidebarEditor @closeEditor="pageBuilderStateStore.setMenuRight(false)">
-        </RightSidebarEditor>
-      </aside>
+          <EditGetElement></EditGetElement>
+          <div
+            @click="pageBuilderStateStore.setComponent(null)"
+            id="contains-pagebuilder"
+            class="pl-4 pr-4 pb-4 overflow-y-auto h-screen pt-1"
+          >
+            <div id="pagebuilder">
+              <div ref="draggableZone">
+                <!-- Added Components to DOM # start -->
+                <div
+                  v-for="component in Array.isArray(getComponents) && getComponents"
+                  :key="component"
+                  id="page-builder-editor-editable-area"
+                  class="bg-white grow"
+                >
+                  <div @mouseup="handleSelectComponent(component)" class="relative group">
+                    <div v-html="component.html_code"></div>
+                  </div>
+                </div>
+              </div>
+              <!-- Added Components to DOM # end -->
+
+              <!-- Add Component # start -->
+              <div
+                class="rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 my-12 mx-8"
+              >
+                <h3 class="mt-2 text-sm font-medium text-gray-900">Add Components</h3>
+                <p class="mt-1 text-sm text-gray-500">
+                  Get started by adding components using the drag & drop Page Builder.
+                </p>
+                <div class="mt-6 flex items-center gap-2 justify-center">
+                  <button
+                    @click="
+                      () => {
+                        pageBuilderStateStore.setComponentArrayAddMethod('push')
+                        handleAddComponent()
+                      }
+                    "
+                    type="button"
+                    class="myPrimaryButton flex items-center gap-2 justify-center"
+                  >
+                    <span class="myMediumIcon material-symbols-outlined"> add </span>
+                    Add component
+                  </button>
+                </div>
+              </div>
+              <!-- Add Component # end -->
+            </div>
+          </div>
+          <!-- Add Component # end -->
+        </main>
+
+        <aside
+          aria-label="Menu"
+          :class="{ 'w-0': !getMenuRight, 'w-80 ml-4': getMenuRight }"
+          class="h-full duration-300 z-20 flex-shrink-0 overflow-hidden shadow-2xl rounded-l-2xl bg-white"
+        >
+          <RightSidebarEditor @closeEditor="pageBuilderStateStore.setMenuRight(false)">
+          </RightSidebarEditor>
+        </aside>
+      </div>
     </div>
   </div>
 </template>
