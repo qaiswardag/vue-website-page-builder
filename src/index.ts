86d7d488da7a38c5a10d74481c26ad7f467a6eee
Qais Wardag <qais.wardag@outlook.com>
2025-06-02 11:07:51 +0300

    refactor: update comments for clarity in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@
 export { default as PageBuilder } from './PageBuilder/PageBuilder.vue'
 export { default as Preview } from './PageBuilder/Preview.vue'
 
-// Export main store
+// Export stores
 export { usePageBuilderStateStore } from './stores/page-builder-state'
+export { useMediaLibraryStore } from './stores/media-library'
+
+// Export PageBuilder composable/class
+export { default as PageBuilderComposable } from './composables/PageBuilder'
 
 // Export CSS (users will need to import this separately)
 import './css/app.css'
