5e275f7d4964a972afab184e1eeade63db2c03fa
Qais Wardag <qais.wardag@outlook.com>
2025-06-06 18:34:28 +0300

    feat: add Footer component to App.vue for improved layout and user navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HomeSection from '@/Components/Homepage/HomeSection.vue'
+import Footer from '@/Components/Homepage/Footer.vue'
 </script>
 
 <template>
   <div>
     <HomeSection></HomeSection>
+    <Footer></Footer>
   </div>
 </template>
