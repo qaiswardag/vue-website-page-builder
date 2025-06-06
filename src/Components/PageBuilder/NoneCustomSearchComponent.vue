d8a1976655d2dd28b22444cba679b765d5eb950a
Qais Wardag <qais.wardag@outlook.com>
2025-06-06 19:36:54 +0300

    refactor: enhance styling and structure in NoneCustomSearchComponent for improved user interaction and readability

--- src/Components/PageBuilder/NoneCustomSearchComponent.vue
+++ src/Components/PageBuilder/NoneCustomSearchComponent.vue
@@ -45,14 +45,14 @@ const convertToComponentObject = function (comp: any): ComponentObject {
         <div
           v-for="helper in componentHelpers"
           :key="helper.title"
-          class="border border-gray-200 rounded-lg p-4 hover:shadow-md transition-shadow cursor-pointer"
+          class="border border-gray-200 overflow-hidden hover:border-myPrimaryLinkColor duration-100 cursor-pointer p-4"
           @click="handleDropComponent(helper)"
         >
           <div class="max-h-72 cursor-pointer object-contain bg-white mx-auto">
             <div class="mr-2" v-html="helper.icon"></div>
-            <h4 class="font-medium">{{ helper.title }}</h4>
+            <h4 class="myPrimaryParagraph text-sm font-normal">{{ helper.title }}</h4>
           </div>
-          <div class="text-sm text-gray-600">
+          <div class="myPrimaryParagraph text-xs font-normal pt-2">
             Click to add {{ helper.title.toLowerCase() }} component
           </div>
         </div>
@@ -66,11 +66,11 @@ const convertToComponentObject = function (comp: any): ComponentObject {
         <div
           v-for="comp in components[0].components.data"
           :key="comp.title"
-          class="border border-gray-200 rounded-lg overflow-hidden hover:shadow-md transition-shadow cursor-pointer"
+          class="border border-gray-200 overflow-hidden hover:border-myPrimaryLinkColor duration-100 cursor-pointer"
           @click="handleDropComponent(convertToComponentObject(comp))"
         >
           <div
-            class="overflow-hidden whitespace-pre-line flex-1 h-auto rounded-lg border border-gray-200 lg:py-10 py-8 px-2"
+            class="overflow-hidden whitespace-pre-line flex-1 h-auto border-b border-gray-200 lg:py-10 py-8 px-2"
           >
             <img
               v-if="comp.cover_image"
@@ -81,7 +81,8 @@ const convertToComponentObject = function (comp: any): ComponentObject {
             <div v-else class="text-gray-500">{{ comp.title }}</div>
           </div>
           <div class="p-3">
-            <h4 class="font-medium">{{ comp.title }}</h4>
+            <h4 class="myPrimaryParagraph text-sm font-normal">{{ comp.title }}</h4>
+            <div class="myPrimaryParagraph text-xs font-normal pt-2">Click to add component</div>
           </div>
         </div>
       </div>
