c751ba75544e0ca0473dec4e04415bfe9e4362dd
Qais Wardag <qais.wardag@outlook.com>
2025-06-08 11:25:34 +0200

    fix: update icon in PageBuilder.vue button for better clarity and user experience

--- src/PageBuilder/PageBuilder.vue
+++ src/PageBuilder/PageBuilder.vue
@@ -443,7 +443,7 @@ onMounted(async () => {
                     @click="pageBuilderStateStore.setMenuRight(true)"
                     class="h-10 w-10 cursor-pointer rounded-full flex items-center border-none justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white focus-visible:ring-0"
                   >
-                    <span class="material-symbols-outlined"> palette </span>
+                    <span class="material-symbols-outlined"> menu </span>
                   </button>
                 </div>
               </div>
