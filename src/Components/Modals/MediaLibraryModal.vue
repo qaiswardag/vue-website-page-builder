03676e4f72d177f30c5146a302e30bd77ba14808
Qais Wardag <qais.wardag@outlook.com>
2025-06-14 19:16:09 +0200

    refactor: remove teleport wrapper from modals for cleaner structure

--- src/Components/Modals/MediaLibraryModal.vue
+++ src/Components/Modals/MediaLibraryModal.vue
@@ -64,72 +64,68 @@ provide('closeMediaLibraryModal', closeMediaLibraryModal)
 </script>
 
 <template>
-  <teleport to="body">
-    <TransitionRoot :show="open" as="template">
-      <Dialog as="div" class="fixed z-30 inset-0 overflow-y-auto sm:px-4" @close="firstButton">
-        <div class="flex items-end justify-center text-center sm:block sm:p-0">
-          <TransitionChild
-            as="template"
-            enter="ease-out duration-100"
-            enter-from="opacity-0"
-            enter-to="opacity-100"
-            leave="ease-in duration-100"
-            leave-from="opacity-100"
-            leave-to="opacity-0"
-          >
-            <DialogOverlay class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-          </TransitionChild>
+  <TransitionRoot :show="open" as="template">
+    <Dialog as="div" class="fixed z-30 inset-0 overflow-y-auto sm:px-4" @close="firstButton">
+      <div class="flex items-end justify-center text-center sm:block sm:p-0">
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-100"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="ease-in duration-100"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <DialogOverlay class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </TransitionChild>
 
-          <!-- This element is to trick the browser into centering the modal contents. -->
-          <span aria-hidden="true" class="hidden sm:inline-block sm:align-middle sm:h-screen"
-            >&#8203;</span
+        <!-- This element is to trick the browser into centering the modal contents. -->
+        <span aria-hidden="true" class="hidden sm:inline-block sm:align-middle sm:h-screen"
+          >&#8203;</span
+        >
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-100"
+          enter-from="opacity-0 scale-95"
+          enter-to="opacity-100 scale-100"
+          leave="ease-in duration-100"
+          leave-from="opacity-100 scale-100"
+          leave-to="opacity-0 scale-95"
+        >
+          <div
+            class="font-sans relative my-2 inline-block align-bottom bg-white rounded-2xl text-left overflow-hidden shadow-xl transform transition-all sm:align-middle sm:max-w-7xl sm:w-full"
           >
-          <TransitionChild
-            as="template"
-            enter="ease-out duration-100"
-            enter-from="opacity-0 scale-95"
-            enter-to="opacity-100 scale-100"
-            leave="ease-in duration-100"
-            leave-from="opacity-100 scale-100"
-            leave-to="opacity-0 scale-95"
-          >
-            <div
-              class="font-sans relative my-2 inline-block align-bottom bg-white rounded-2xl text-left overflow-hidden shadow-xl transform transition-all sm:align-middle sm:max-w-7xl sm:w-full"
-            >
-              <div
-                class="flex gap-2 justify-between items-center border-b border-gray-200 p-4 mb-2"
-              >
-                <DialogTitle as="h3" class="tertiaryHeader my-0 py-0">
-                  {{ title }}
-                </DialogTitle>
+            <div class="flex gap-2 justify-between items-center border-b border-gray-200 p-4 mb-2">
+              <DialogTitle as="h3" class="tertiaryHeader my-0 py-0">
+                {{ title }}
+              </DialogTitle>
 
+              <div class="flex-end">
                 <div class="flex-end">
-                  <div class="flex-end">
-                    <button
-                      type="button"
-                      class="h-10 w-10 cursor-pointer rounded-full flex items-center border-none justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white hover:fill-white focus-visible:ring-0 text-myPrimaryDarkGrayColor"
-                      @click="firstButton"
-                      aria-label="Close"
-                    >
-                      <span class="material-symbols-outlined"> close </span>
-                    </button>
-                  </div>
-                </div>
-              </div>
-
-              <div class="p-4">
-                <!-- Show only custom media component if provided -->
-                <div v-if="customMediaComponent" class="w-full">
-                  <component :is="customMediaComponent" />
-                </div>
-                <div v-else>
-                  <DefaultMediaLibraryComponent />
+                  <button
+                    type="button"
+                    class="h-10 w-10 cursor-pointer rounded-full flex items-center border-none justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white hover:fill-white focus-visible:ring-0 text-myPrimaryDarkGrayColor"
+                    @click="firstButton"
+                    aria-label="Close"
+                  >
+                    <span class="material-symbols-outlined"> close </span>
+                  </button>
                 </div>
               </div>
             </div>
-          </TransitionChild>
-        </div>
-      </Dialog>
-    </TransitionRoot>
-  </teleport>
+
+            <div class="p-4">
+              <!-- Show only custom media component if provided -->
+              <div v-if="customMediaComponent" class="w-full">
+                <component :is="customMediaComponent" />
+              </div>
+              <div v-else>
+                <DefaultMediaLibraryComponent />
+              </div>
+            </div>
+          </div>
+        </TransitionChild>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
--- src/Components/Modals/PageBuilderPreviewModal.vue
+++ src/Components/Modals/PageBuilderPreviewModal.vue
@@ -36,85 +36,83 @@ const getConfigPageBuilder = computed(() => {
 </script>
 
 <template>
-  <teleport to="body">
-    <TransitionRoot :show="show" as="template">
-      <Dialog
-        @close="firstButton"
-        @keydown.escape.prevent="handleEscapeKey"
-        as="div"
-        class="fixed z-30 inset-0 overflow-y-auto focus:outline-none"
-        tabindex="0"
+  <TransitionRoot :show="show" as="template">
+    <Dialog
+      @close="firstButton"
+      @keydown.escape.prevent="handleEscapeKey"
+      as="div"
+      class="fixed z-30 inset-0 overflow-y-auto focus:outline-none"
+      tabindex="0"
+    >
+      <div
+        class="font-sans flex items-end justify-center pb-20 text-center sm:block sm:p-0 bg-white"
       >
-        <div
-          class="font-sans flex items-end justify-center pb-20 text-center sm:block sm:p-0 bg-white"
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-100"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="ease-in duration-100"
+          leave-from="opacity-100"
+          leave-to="opacity-100"
         >
-          <TransitionChild
-            as="template"
-            enter="ease-out duration-100"
-            enter-from="opacity-0"
-            enter-to="opacity-100"
-            leave="ease-in duration-100"
-            leave-from="opacity-100"
-            leave-to="opacity-100"
-          >
-            <DialogOverlay class="fixed inset-0 bg-opacity-75 transition-opacity" />
-          </TransitionChild>
+          <DialogOverlay class="fixed inset-0 bg-opacity-75 transition-opacity" />
+        </TransitionChild>
 
-          <!-- This element is to trick the browser into centering the modal contents. -->
-          <span aria-hidden="true" class="hidden sm:inline-block sm:align-middle sm:h-screen"
-            >&#8203;</span
-          >
-          <TransitionChild
-            as="template"
-            enter="ease-out duration-100"
-            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enter-to="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-100"
-            leave-from="opacity-100 translate-y-0 sm:scale-100"
-            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        <!-- This element is to trick the browser into centering the modal contents. -->
+        <span aria-hidden="true" class="hidden sm:inline-block sm:align-middle sm:h-screen"
+          >&#8203;</span
+        >
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-100"
+          enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          enter-to="opacity-100 translate-y-0 sm:scale-100"
+          leave="ease-in duration-100"
+          leave-from="opacity-100 translate-y-0 sm:scale-100"
+          leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        >
+          <div
+            class="bg-red-100 inline-block align-bottom text-left transform transition-all sm:align-middle w-full overflow-hidden h-[100vh] top-0 left-0 right-0 absolute"
           >
             <div
-              class="bg-red-100 inline-block align-bottom text-left transform transition-all sm:align-middle w-full overflow-hidden h-[100vh] top-0 left-0 right-0 absolute"
+              class="px-4 lg:h-[10vh] h-[16vh] flex items-center justify-between border-b border-gray-200 bg-white"
             >
               <div
-                class="px-4 lg:h-[10vh] h-[16vh] flex items-center justify-between border-b border-gray-200 bg-white"
+                v-if="
+                  getConfigPageBuilder &&
+                  getConfigPageBuilder.pageBuilderLogo &&
+                  getConfigPageBuilder.pageBuilderLogo.src
+                "
+                class="flex items-center divide-x divide-gray-200"
               >
-                <div
-                  v-if="
-                    getConfigPageBuilder &&
-                    getConfigPageBuilder.pageBuilderLogo &&
-                    getConfigPageBuilder.pageBuilderLogo.src
-                  "
-                  class="flex items-center divide-x divide-gray-200"
-                >
-                  <div class="pr-4">
-                    <img class="h-6" :src="getConfigPageBuilder.pageBuilderLogo.src" alt="Logo" />
-                  </div>
-                  <span class="myPrimaryParagraph font-medium pl-4">Preview mode </span>
+                <div class="pr-4">
+                  <img class="h-6" :src="getConfigPageBuilder.pageBuilderLogo.src" alt="Logo" />
                 </div>
-                <div v-else>
-                  <div class="pr-6">
-                    <span class="myPrimaryParagraph font-medium">Preview mode </span>
-                  </div>
-                </div>
-
-                <div
-                  @click="firstButton"
-                  class="flex items-center justify-center gap-1 cursor-pointer"
-                >
-                  <span class="myPrimaryParagraph font-medium"> Close Preview </span>
-                  <div
-                    class="h-10 w-10 cursor-pointer rounded-full flex items-center border-none justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white focus-visible:ring-0"
-                  >
-                    <span class="material-symbols-outlined"> close </span>
-                  </div>
+                <span class="myPrimaryParagraph font-medium pl-4">Preview mode </span>
+              </div>
+              <div v-else>
+                <div class="pr-6">
+                  <span class="myPrimaryParagraph font-medium">Preview mode </span>
                 </div>
               </div>
-              <slot></slot>
+
+              <div
+                @click="firstButton"
+                class="flex items-center justify-center gap-1 cursor-pointer"
+              >
+                <span class="myPrimaryParagraph font-medium"> Close Preview </span>
+                <div
+                  class="h-10 w-10 cursor-pointer rounded-full flex items-center border-none justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white focus-visible:ring-0"
+                >
+                  <span class="material-symbols-outlined"> close </span>
+                </div>
+              </div>
             </div>
-          </TransitionChild>
-        </div>
-      </Dialog>
-    </TransitionRoot>
-  </teleport>
+            <slot></slot>
+          </div>
+        </TransitionChild>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
