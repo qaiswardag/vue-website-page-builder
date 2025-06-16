4bc7126bc84f94f765dce2f535a220791cbaa735
Qais Wardag <qais.wardag@outlook.com>
2025-06-17 01:09:51 +0200

    feat: add loading state for image processing in ImageEditor component

--- src/Components/PageBuilder/EditorMenu/Editables/ImageEditor.vue
+++ src/Components/PageBuilder/EditorMenu/Editables/ImageEditor.vue
@@ -3,13 +3,17 @@ import { computed, ref, inject } from 'vue'
 import MediaLibraryModal from '../../../Modals/MediaLibraryModal.vue'
 import PageBuilderClass from '../../../../composables/PageBuilderClass.ts'
 import { sharedPageBuilderStore } from '../../../../stores/shared-store'
+import { preloadImage } from '../../../../composables/preloadImage'
+import { delay } from '../../../../composables/delay'
 
 // Use shared store instance
 const pageBuilderStateStore = sharedPageBuilderStore
 const customMediaComponent = inject('CustomMediaComponent')
 
 const pageBuilderClass = new PageBuilderClass(pageBuilderStateStore)
+
 const isLoading = ref(false)
+const getIsLoadingImage = ref(false)
 
 // use media library
 const showMediaLibraryModal = ref(false)
@@ -24,6 +28,9 @@ const firstMediaButtonFunction = ref(null)
 
 // get current image from store
 const getBasePrimaryImage = computed(() => {
+  if (pageBuilderStateStore.getBasePrimaryImage) {
+    loadingImage(pageBuilderStateStore.getBasePrimaryImage)
+  }
   return pageBuilderStateStore.getBasePrimaryImage
 })
 
@@ -45,10 +52,35 @@ const handleAddImage = function () {
   //
   // end modal
 }
+
+const loadingImage = async function (imageURL) {
+  getIsLoadingImage.value = true
+  if (imageURL && typeof imageURL === 'string' && imageURL.length > 2) {
+    try {
+      await preloadImage(imageURL)
+    } catch (err) {
+      console.log('Unable getting the image:', err)
+    } finally {
+      getIsLoadingImage.value = false
+    }
+  }
+}
 </script>
 <template>
   <div>
-    <div v-if="getBasePrimaryImage !== null">
+    <div v-show="getIsLoadingImage">
+      <div class="flex items-center justify-center mt-4 min-h-80">
+        <div
+          class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        >
+          <span
+            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...</span
+          >
+        </div>
+      </div>
+    </div>
+    <div v-show="getBasePrimaryImage && !getIsLoadingImage">
       <img
         class="object-cover object-center w-full cursor-pointer"
         :src="getBasePrimaryImage"
