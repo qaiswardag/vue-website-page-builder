3aed3dc07bb0a18743155614c81033fb8a8e4b17
Qais Wardag <qais.wardag@outlook.com>
2025-06-19 16:41:16 +0200

    fix: reduce delay time for auto-save functionality in PageBuilderClass

--- src/composables/PageBuilderClass.ts
+++ src/composables/PageBuilderClass.ts
@@ -327,7 +327,7 @@ class PageBuilderClass {
       ) {
         this.pageBuilderStateStore.setIsSaving(true)
         await this.saveComponentsLocalStorage()
-        await this.delay(5000)
+        await this.delay(500)
 
         this.pageBuilderStateStore.setIsSaving(false)
       }
