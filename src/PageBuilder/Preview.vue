4b71cc47ea4b2393edf1ac8d664b84460a5a7aa8
Qais Wardag <qais.wardag@outlook.com>
2025-07-02 17:30:06 +0200

    fix: enhance mobile preview functionality by adding iframe support and updating styles

--- src/PageBuilder/Preview.vue
+++ src/PageBuilder/Preview.vue
@@ -1,13 +1,14 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watchEffect } from 'vue'
 
-defineProps({
+const props = defineProps({
   mobile: {
     type: Boolean,
   },
 })
 
 const htmlPage = ref('')
+const iframeRef = ref(null)
 
 const previewData = localStorage.getItem('preview')
 
@@ -20,6 +21,23 @@ if (previewData) {
     htmlPage.value = ''
   }
 }
+
+watchEffect(() => {
+  if (props.mobile && iframeRef.value && htmlPage.value) {
+    const iframe = iframeRef.value
+    const doc = iframe.contentWindow.document
+    doc.open()
+    doc.write(
+      `<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body>${htmlPage.value}</body></html>`,
+    )
+    doc.close()
+
+    // Copy stylesheets
+    document.querySelectorAll('link[rel="stylesheet"], style').forEach((node) => {
+      doc.head.appendChild(node.cloneNode(true))
+    })
+  }
+})
 </script>
 
 <template>
@@ -35,14 +53,12 @@ if (previewData) {
     </div>
   </template>
   <template v-if="mobile">
-    <div>
-      <div
-        class="pbx-text-black pbx-w-full pbx-inset-x-0 pbx-h-[70vh] pbx-bg-white pbx-overflow-x-scroll lg:pbx-pt-2 pbx-pt-2"
-      >
-        <div id="page-builder-editor">
-          <div class="" v-html="htmlPage"></div>
-        </div>
-      </div>
+    <div class="pbx-bg-gray-200 pbx-p-4">
+      <iframe
+        ref="iframeRef"
+        class="pbx-mx-auto pbx-w-full pbx-bg-white pbx-shadow-lg pbx-h-[80vh] pbx-border-0"
+        src="about:blank"
+      ></iframe>
     </div>
   </template>
 </template>
