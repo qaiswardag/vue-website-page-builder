5dd1f5343eec563da7f82c4b51c7fb6eee9a8b51
Qais Wardag <qais.wardag@outlook.com>
2025-07-06 00:06:53 +0200

    feat: add translations for "Opacity & Transparency" in multiple languages; update OpacityEditor.vue and locale files

--- src/Components/PageBuilder/EditorMenu/Editables/OpacityEditor.vue
+++ src/Components/PageBuilder/EditorMenu/Editables/OpacityEditor.vue
@@ -2,11 +2,14 @@
 import EditorAccordion from '../EditorAccordion.vue'
 import ManageBackgroundOpacity from './ManageBackgroundOpacity.vue'
 import ManageOpacity from './ManageOpacity.vue'
+import { useTranslations } from '../../../../composables/useTranslations'
+
+const { translate } = useTranslations()
 </script>
 
 <template>
   <EditorAccordion>
-    <template #title>Opacity & Transparency </template>
+    <template #title>{{ translate('Opacity & Transparency') }}</template>
     <template #content>
       <ManageOpacity></ManageOpacity>
       <hr />
