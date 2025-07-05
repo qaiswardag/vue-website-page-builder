96c994167f089d8b90692f8cbab1817373fe9e8c
Qais Wardag <qais.wardag@outlook.com>
2025-07-05 23:57:38 +0200

    feat: add translations for typography-related terms in multiple languages; update Typography.vue and locale files

--- src/Components/PageBuilder/EditorMenu/Editables/Typography.vue
+++ src/Components/PageBuilder/EditorMenu/Editables/Typography.vue
@@ -5,6 +5,10 @@ import tailwindFontSizes from '../../../../utils/builder/tailwind-font-sizes'
 import tailwindFontStyles from '../../../../utils/builder/tailwind-font-styles'
 import { sharedPageBuilderStore } from '../../../../stores/shared-store'
 import { getPageBuilder } from '../../../../composables/builderInstance'
+
+import { useTranslations } from '../../../../composables/useTranslations'
+
+const { translate } = useTranslations()
 const pageBuilderService = getPageBuilder()
 
 // Use shared store instance
@@ -99,20 +103,24 @@ watch(
 
 <template>
   <EditorAccordion>
-    <template #title> Typographies </template>
+    <template #title>{{ translate('Typographies') }}</template>
     <template #content>
       <!-- FONT SIZES -->
-      <p class="pbx-myPrimaryParagraph pbx-font-medium pbx-py-0 pbx-my-4">Font Appearance</p>
+      <p class="pbx-myPrimaryParagraph pbx-font-medium pbx-py-0 pbx-my-4">
+        {{ translate('Font Appearance') }}
+      </p>
       <template v-if="false">
         <div class="pbx-my-2 pbx-py-2">
-          <label for="font-base" class="pbx-myPrimaryInputLabel"> Font Size </label>
+          <label for="font-base" class="pbx-myPrimaryInputLabel">
+            {{ translate('Font Size') }}
+          </label>
           <select
             id="font-base"
             v-model="fontBase"
             class="pbx-myPrimarySelect"
             @change="pageBuilderService.handleFontSizeBase(fontBase)"
           >
-            <option disabled value="">Select</option>
+            <option disabled value="">{{ translate('Select') }}</option>
             <option v-for="fontSize in tailwindFontSizes.fontBase" :key="fontSize">
               {{ fontSize }}
             </option>
@@ -120,14 +128,16 @@ watch(
         </div>
       </template>
       <div class="pbx-my-2 pbx-py-2">
-        <label for="font-desktop" class="pbx-myPrimaryInputLabel"> Font size </label>
+        <label for="font-desktop" class="pbx-myPrimaryInputLabel">
+          {{ translate('Font size') }}
+        </label>
         <select
           id="font-desktop"
           v-model="fontDesktop"
           class="pbx-myPrimarySelect"
           @change="pageBuilderService.handleFontSizeDesktop(fontDesktop)"
         >
-          <option disabled value="">Select</option>
+          <option disabled value="">{{ translate('Select') }}</option>
           <option v-for="fontSize in tailwindFontSizes.fontDesktop" :key="fontSize">
             {{ fontSize }}
           </option>
@@ -135,28 +145,32 @@ watch(
       </div>
       <template v-if="false">
         <div class="pbx-my-2 pbx-py-2">
-          <label for="font-tablet" class="pbx-myPrimaryInputLabel"> Font tablet size </label>
+          <label for="font-tablet" class="pbx-myPrimaryInputLabel">
+            {{ translate('Font tablet size') }}
+          </label>
           <select
             id="font-tablet"
             v-model="fontTablet"
             class="pbx-myPrimarySelect"
             @change="pageBuilderService.handleFontSizeTablet(fontTablet)"
           >
-            <option disabled value="">Select</option>
+            <option disabled value="">{{ translate('Select') }}</option>
             <option v-for="fontSize in tailwindFontSizes.fontTablet" :key="fontSize">
               {{ fontSize }}
             </option>
           </select>
         </div>
         <div class="pbx-my-2 pbx-py-2">
-          <label for="font-mobile" class="pbx-myPrimaryInputLabel"> Font small screens </label>
+          <label for="font-mobile" class="pbx-myPrimaryInputLabel">
+            {{ translate('Font small screens') }}
+          </label>
           <select
             id="font-mobile"
             v-model="fontMobile"
             class="pbx-myPrimarySelect"
             @change="pageBuilderService.handleFontSizeMobile(fontMobile)"
           >
-            <option disabled value="">Select</option>
+            <option disabled value="">{{ translate('Select') }}</option>
             <option v-for="fontSize in tailwindFontSizes.fontMobile" :key="fontSize">
               {{ fontSize }}
             </option>
@@ -166,14 +180,16 @@ watch(
       <hr />
 
       <div class="pbx-my-2 pbx-py-2">
-        <label for="font-weight" class="pbx-myPrimaryInputLabel"> Font weight </label>
+        <label for="font-weight" class="pbx-myPrimaryInputLabel">
+          {{ translate('Font weight') }}
+        </label>
         <select
           id="font-weight"
           v-model="fontWeight"
           class="pbx-myPrimarySelect"
           @change="pageBuilderService.handleFontWeight(fontWeight)"
         >
-          <option disabled value="">Select</option>
+          <option disabled value="">{{ translate('Select') }}</option>
           <option v-for="fontWeight in tailwindFontStyles.fontWeight" :key="fontWeight">
             {{ fontWeight }}
           </option>
@@ -183,14 +199,16 @@ watch(
 
       <!-- FONT FAMILY -->
       <div class="pbx-my-2 pbx-py-2">
-        <label for="font-family" class="pbx-myPrimaryInputLabel"> Font family </label>
+        <label for="font-family" class="pbx-myPrimaryInputLabel">
+          {{ translate('Font family') }}
+        </label>
         <select
           id="font-family"
           v-model="fontFamily"
           class="pbx-myPrimarySelect"
           @change="pageBuilderService.handleFontFamily(fontFamily)"
         >
-          <option disabled value="">Select</option>
+          <option disabled value="">{{ translate('Select') }}</option>
           <option v-for="fontFamily in tailwindFontStyles.fontFamily" :key="fontFamily">
             {{ fontFamily }}
           </option>
@@ -199,14 +217,16 @@ watch(
       <hr />
 
       <div class="pbx-my-2 pbx-py-2">
-        <label for="font-style" class="pbx-myPrimaryInputLabel"> Font Style </label>
+        <label for="font-style" class="pbx-myPrimaryInputLabel">
+          {{ translate('Font Style') }}
+        </label>
         <select
           id="font-style"
           v-model="fontStyle"
           class="pbx-myPrimarySelect"
           @change="pageBuilderService.handleFontStyle(fontStyle)"
         >
-          <option disabled value="">Select</option>
+          <option disabled value="">{{ translate('Select') }}</option>
           <option v-for="fontStyle in tailwindFontStyles.fontStyle" :key="fontStyle">
             {{ fontStyle }}
           </option>
