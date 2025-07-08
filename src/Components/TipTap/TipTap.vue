17bb42934d712b99104fc13708a26b7d888fd778
Qais Wardag <47112831+qaiswardag@users.noreply.github.com>
2025-07-08 18:39:58 +0200

    Fix/minor bug fixes 8 (#61)
    * Fix/version bump to 3.3.91
    
    * Fix/tip tap component background color
    
    * Fix/layout adjustments for navbar and main sections
    
    * Fix/layout adjustments for page builder component widths
    
    * Fix/typo corrections in README for clarity and consistency
    
    * Fix/documentation updates for builderInstance.ts to enhance clarity and consistency
    
    * Fix/image updates in README for improved visual consistency
    
    * Fix/layout adjustments for page builder component widths to enhance responsiveness
    
    * Fix/update image path in README for consistency and accuracy
    
    * Refactor PageBuilderService: Enhance documentation and remove unused media files
    
    - Deleted unused media files: media_library.jpg, page_builder_architecture.png, and text_editor.jpg.
    - Improved code documentation throughout PageBuilderService.ts, adding detailed JSDoc comments for methods and parameters.
    - Ensured clarity in method descriptions, including async operations and their expected outcomes.
    - Refined logic in methods related to component management, styling, and event handling for better maintainability.

--- src/Components/TipTap/TipTap.vue
+++ src/Components/TipTap/TipTap.vue
@@ -12,7 +12,7 @@ const pageBuilderService = getPageBuilder()
       <div class="pbx-flex pbx-flex-col pbx-border-solid pbx-border pbx-border-gray-400">
         <div
           @click="pageBuilderService.toggleTipTapModal(true)"
-          class="pbx-flex pbx-flex-row pbx-justify-between pbx-items-center pbx-pl-3 pbx-pr-3 pbx-py-5 pbx-cursor-pointer pbx-duration-200 hover:pbx-bg-myPrimaryLightGrayColor"
+          class="pbx-flex pbx-flex-row pbx-justify-between pbx-items-center pbx-pl-3 pbx-pr-3 pbx-py-5 pbx-cursor-pointer pbx-duration-200 hover:pbx-bg-myPrimaryLightGrayColor pbx-bg-white"
         >
           <p class="pbx-myPrimaryParagraph pbx-font-medium pbx-my-0 pbx-py-0">
             {{ translate('Text') }}
