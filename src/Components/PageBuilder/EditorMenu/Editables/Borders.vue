08bd474fa83f389f3467b0cc539ee75da63c0e54
Qais Wardag <qais.wardag@outlook.com>
2025-07-13 16:21:48 +0200

    Refactor: Remove unnecessary dropdown indicators from Listbox components and enhance input styles

--- src/Components/PageBuilder/EditorMenu/Editables/Borders.vue
+++ src/Components/PageBuilder/EditorMenu/Editables/Borders.vue
@@ -121,11 +121,6 @@ watch(
                 ></div>
                 <span class="pbx-block pbx-truncate">{{ borderColor }}</span>
               </span>
-              <span
-                class="pbx-pointer-events-none pbx-absolute pbx-inset-y-0 pbx-right-0 pbx-ml-3 pbx-flex pbx-items-center pbx-pr-2"
-              >
-                <span class="material-symbols-outlined"> keyboard_arrow_down </span>
-              </span>
             </ListboxButton>
 
             <transition
